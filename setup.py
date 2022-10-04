a7f79313f018d75fee3f5e88eac9e68ed8db4c4a
Newell, Mark <mark.newell1@mclaren.com>
2022-10-04 19:56:39 +0100

    Added setup-tools dependancies

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     packages=['gec'],
     install_requires=[
         "pytorch_pretrained_bert",
-        "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz",
         "torch==1.11.0",
         "numpy",
         "cdifflib",
         "keras",
         "spacy==2.1.0"
+    ],
+    dependency_links=[
+        "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz",
     ],
 )
